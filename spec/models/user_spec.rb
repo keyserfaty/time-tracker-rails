c204ed8a926dda2c1734485efae1447417591333
keyserfaty <keserfaty@gmail.com>
2018-08-28 14:30:27 -0300

    add validation to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "creation" do
+    before do
+      @user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: "Jon", last_name: "Snow")
+    end
+    
     it "can be created" do
-      user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh')
-      expect(user).to be_valid
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created with first_name last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+
+      expect(@user).to_not be_valid
     end
   end
 end
