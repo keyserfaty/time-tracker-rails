ac97c3fd8ef32d4a92bc0d11c74f6765fdb79b2e
keyserfaty <keserfaty@gmail.com>
2018-08-28 14:44:19 -0300

    add posts view and tests

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'index' do
+    it 'can be reached successfully' do
+      visit posts_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
