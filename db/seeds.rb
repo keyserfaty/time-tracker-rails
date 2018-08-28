b90077f4faf929a6ddfa22fb484ab8f373df3dd4
keyserfaty <keserfaty@gmail.com>
2018-08-28 16:33:47 -0300

    update list view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: 'Jon', last_name: 'Snow')
+
+p "1 user created"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 p "100 posts created"
