c560611d78ae54e92198fd7a9937603feff465ff
keyserfaty <keserfaty@gmail.com>
2018-08-28 16:26:08 -0300

    fix tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
+    @user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: 'Jon', last_name: 'Snow')
+    login_as(@user, :scope => :user)
   end
-  
+
   describe 'index' do
     before do
-      user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: 'Jon', last_name: 'Snow')
-      login_as(user, :scope => :user)
       visit posts_path  # this is coming from the name of the routes
     end
     
@@ -20,8 +20,8 @@ describe 'navigate' do
     end
     
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1")
-      post2 = Post.create(date: Date.today, rationale: "Post2")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       p post1
       
       visit posts_path
