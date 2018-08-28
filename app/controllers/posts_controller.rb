e44c3d926dfbf8b5f0b91e28b4c802ec82450e09
keyserfaty <keserfaty@gmail.com>
2018-08-28 15:14:21 -0300

    refactor posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
   end
 
@@ -17,12 +19,15 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params
     params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
