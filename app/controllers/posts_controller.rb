6ecb18518c702841d474b5750b0dd9e08761efbf
keyserfaty <keserfaty@gmail.com>
2018-08-28 15:08:39 -0300

    add post creation and show views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale)) # it's checking the content to avoid injection
+    @post.save # saves to the db
+
+    redirect_to @post # just like that redirects the the view of the post just created
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
