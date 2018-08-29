7aed874fcf698501948a6b65433f3154b3b575ac
keyserfaty <keserfaty@gmail.com>
2018-08-29 18:43:43 -0300

    small refactor

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -34,6 +34,10 @@ class ApplicationPolicy
     false
   end
 
+  def admin_types
+    %w(AdminUser)
+  end
+
   class Scope
     attr_reader :user, :scope
 
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,5 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id
+    record.user_id == user.id || admin_types.includes?(user.type)
   end
 end
