a38917d4996dd2bff1d24369e336221bd4ce5f97
keyserfaty <keserfaty@gmail.com>
2018-08-29 17:47:34 -0300

    add status to admin view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 @user = User.create(email: 'test@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: 'Jon', last_name: 'Snow')
+@admin_user = AdminUser.create(email: 'admin@test.com', password: 'asdfgh', password_confirmation: 'asdfgh', first_name: 'Jon', last_name: 'Snow')
 
-p "1 user created"
+p "2 users created"
 
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
